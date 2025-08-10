26074ede4a7af1332b9f70050aa21d80f438a184
Danillo Silva <danillordm@gmail.com>
2025-08-10 16:54:45 -0300

    Remove anotações @Query desnecessárias de ClientePFRepository e ClientePJRepository

--- celula-financeiro-controladoria/src/main/java/br/com/desafio/celula_financeiro_controladoria/domain/repository/cliente/ClientePFRepository.java
+++ celula-financeiro-controladoria/src/main/java/br/com/desafio/celula_financeiro_controladoria/domain/repository/cliente/ClientePFRepository.java
@@ -3,13 +3,11 @@ package br.com.desafio.celula_financeiro_controladoria.domain.repository.cliente
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.desafio.celula_financeiro_controladoria.domain.entity.cliente.ClientePF;
 
 @Repository
 public interface ClientePFRepository extends JpaRepository<ClientePF, Long> {
-    @Query("SELECT c FROM ClientePF c WHERE c.cpf = :cpf")
     Optional<ClientePF> findByCpf(String cpf);
 }
--- celula-financeiro-controladoria/src/main/java/br/com/desafio/celula_financeiro_controladoria/domain/repository/cliente/ClientePJRepository.java
+++ celula-financeiro-controladoria/src/main/java/br/com/desafio/celula_financeiro_controladoria/domain/repository/cliente/ClientePJRepository.java
@@ -3,13 +3,11 @@ package br.com.desafio.celula_financeiro_controladoria.domain.repository.cliente
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import br.com.desafio.celula_financeiro_controladoria.domain.entity.cliente.ClientePF;
+import br.com.desafio.celula_financeiro_controladoria.domain.entity.cliente.ClientePJ;
 
 @Repository
-public interface ClientePJRepository extends JpaRepository<ClientePF, Long> {
-    @Query("SELECT c FROM ClientePF c WHERE c.cnpj = :cnpj")
-    Optional<ClientePF> findByCnpj(String cnpj);
+public interface ClientePJRepository extends JpaRepository<ClientePJ, Long> {
+    Optional<ClientePJ> findByCnpj(String cnpj);
 }
