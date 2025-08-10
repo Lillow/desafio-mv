26c0d51a13cb9cbb8f33db100a78daa968f4515c
Danillo Silva <danillordm@gmail.com>
2025-08-10 19:43:57 -0300

    Adiciona anotações @Repository nas interfaces ContaRepository, EnderecoRepository e MovimentoRepository para melhorar a clareza e a organização do código.

--- celula-financeiro-controladoria/src/main/java/br/com/desafio/celula_financeiro_controladoria/domain/repository/ContaRepository.java
+++ celula-financeiro-controladoria/src/main/java/br/com/desafio/celula_financeiro_controladoria/domain/repository/ContaRepository.java
@@ -2,9 +2,11 @@ package br.com.desafio.celula_financeiro_controladoria.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import br.com.desafio.celula_financeiro_controladoria.domain.entity.Conta;
 
+@Repository
 public interface ContaRepository extends JpaRepository<Conta, Long> {
 
     @Query("select count(m)>0 from Movimento m where m.conta.id = :contaId")
--- celula-financeiro-controladoria/src/main/java/br/com/desafio/celula_financeiro_controladoria/domain/repository/EnderecoRepository.java
+++ celula-financeiro-controladoria/src/main/java/br/com/desafio/celula_financeiro_controladoria/domain/repository/EnderecoRepository.java
@@ -3,9 +3,11 @@ package br.com.desafio.celula_financeiro_controladoria.domain.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.desafio.celula_financeiro_controladoria.domain.entity.Endereco;
 
+@Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 
     Optional<Endereco> findByClienteId(Long clienteId);
