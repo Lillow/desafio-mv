842fba2d56fbaf2c0d8e4d0d40c1dc81dd71748b
Danillo Silva <danillordm@gmail.com>
2025-08-10 17:58:54 -0300

    Adiciona arquivos de configuração para H2 e Oracle, além de refatorar DemoLoader e application.properties para melhor gerenciamento de perfis e dependências.

--- celula-financeiro-controladoria/config/DemoLoader.java
+++ celula-financeiro-controladoria/config/DemoLoader.java
@@ -4,10 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("h2")
 public class DemoLoader implements CommandLineRunner {
-
     @Override
     public void run(String... args) {
-        // Inserir clientes, contas e movimentações de exemplo
+        // TODO: inserir clientes, contas e movimentações de exemplo
+        // Ex: repositories.save(...); etc.
     }
 }
