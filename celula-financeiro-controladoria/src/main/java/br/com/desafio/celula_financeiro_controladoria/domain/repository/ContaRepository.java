2b421fd2919968beb5348399d24cefc52b6150a3
Danillo Silva <danillordm@gmail.com>
2025-08-10 16:54:17 -0300

    Refatora ContaRepository removendo anotações @Query desnecessárias

--- celula-financeiro-controladoria/src/main/java/br/com/desafio/celula_financeiro_controladoria/domain/repository/ContaRepository.java
+++ celula-financeiro-controladoria/src/main/java/br/com/desafio/celula_financeiro_controladoria/domain/repository/ContaRepository.java
@@ -5,17 +5,16 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import br.com.desafio.celula_financeiro_controladoria.domain.entity.Conta;
 
+@Repository
 public interface ContaRepository extends JpaRepository<Conta, Long> {
 
-    @Query("SELECT c FROM Conta c WHERE c.agencia = :agencia AND c.numero = :numero")
     Optional<Conta> findByAgenciaAndNumero(String agencia, String numero);
 
-    @Query("SELECT c FROM Conta c WHERE c.cliente.id = :clienteId")
     List<Conta> findByClienteId(Long clienteId);
 
-    @Query("SELECT c FROM Conta c WHERE c.documento = :documento")
     List<Conta> findByDocumento(String documento);
 }
